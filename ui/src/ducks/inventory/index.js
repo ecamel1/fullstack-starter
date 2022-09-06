b876dbe49ed69d4142b4c4085bc907b2d9fd2dc9
ecamel1 <ecamel1@students.towson.edu>
2022-09-05 21:57:02 -0400

    implementing actions to hook using axios

--- ui/src/ducks/inventory/index.js
+++ ui/src/ducks/inventory/index.js
@@ -2,6 +2,11 @@ import { createAction, handleActions } from 'redux-actions'
 
 const actions = {
   INVENTORY_GET_ALL: 'inventory/get_all',
+  CREATE: 'inventory/create',
+  UPDATE: 'inventory/update',
+  DELETE: 'inventory/delete',
+  _PENDING: 'inventory/pending',
+  REFRESH:  'inventory/refresh'
 }
 
 export let defaultState = {
