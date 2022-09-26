e4ce45296c62f1045a08104a127c04d19915ec43
ecamel1 <ecamel1@students.towson.edu>
2022-09-26 11:12:33 -0400

    outline for InventoryFormModal

---
+++ ui/src/components/Inventory/InventoryFormModal.js
@@ -0,0 +1,17 @@
+import Button from '@material-ui/core/Button'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import Grid from '@material-ui/core/Grid'
+import React from 'react'
+import TextField from '../Form/TextField'
+import { Field, Form, Formik } from 'formik'
+
+class InventoryFormModal extends React.Component {
+	render(){
+
+	}
+}
+
+export default InventoryFormModal
